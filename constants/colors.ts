90241909ea4bbcdde934a19d329769519067d053
firstChairCoder <md1qq7@gmail.com>
2023-09-07 13:29:52 +0100

    chore(pkg): install fonts

---
+++ constants/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  white: "#FFFFFF",
+  cyan: "#2CB9B0",
+  lightCyan: "#E7F9F7",
+  darkBlue: "#0C0D34",
+  orange: "#FE5E33",
+  yellow: "#FFC641",
+  pink: "#FF87A2",
+  darkPink: "#FF0058",
+  violet: "#442CB9",
+  lightBlue: "#BFEAF5",
+  grey: "#F4F0EF",
+  darkGrey: "#808080",
+};
